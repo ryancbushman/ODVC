d3c4ff3035c1857d99a4e1102783888b11a05144
ryancbushman <ryan.c.bushman@gmail.com>
2024-01-10 11:40:30 -0700

    documenting datasets

---
+++ R/two_way_dataset_doc.R
@@ -0,0 +1,170 @@
+#' Datasets of two-way nested random effects designs generated from
+#' \eqn{\mathcal{C}_{3,2}} and \eqn{\mathcal{C}_{3,3}}.
+#'
+#' @format ## `two-way datasets`
+#' dataframes of designs from one of the two aforementioned classes with
+#' prespecified number of observations and values for \eqn{rho_1} and
+#' \eqn{rho_2}
+#'
+#' \describe{
+#'    \item{N}{Number of total observations}
+#'    \item{N_i_dot}{number of total observations within group i}
+#'    \item{N_ij}{number of observations within each subgroup}
+#'    \item{D_score}{The determinant of the inverse information matrix}
+#'    \item{Relative_D_Efficiency}{Efficiency compared to the D optimal design}
+#'    \item{A_score}{The trace of the inverse information matrix}
+#'    \item{Relative_A_Efficiency}{Efficiency compared to the A optimal design}
+#'    }
+#'
+#' "N12_0.1_0.1"
+#'
+#' @rdname
+#' "N12_0.1_0.5"
+#' @rdname
+#' "N12_0.1_1"
+#' @rdname
+#' "N12_0.1_2"
+#' @rdname
+#' "N12_0.1_10"
+#' @rdname
+#' "N12_0.5_0.1"
+#' @rdname
+#' "N12_0.5_0.5"
+#' @rdname
+#' "N12_0.5_1"
+#' @rdname
+#' "N12_0.5_2"
+#' @rdname
+#' "N12_0.5_10"
+#' @rdname
+#' "N12_1_0.1"
+#' @rdname
+#' "N12_1_0.5"
+#' @rdname
+#' "N12_1_1"
+#' @rdname
+#' "N12_1_2"
+#' @rdname
+#' "N12_1_10"
+#' @rdname
+#' "N12_2_0.1"
+#' @rdname
+#' "N12_2_0.5"
+#' @rdname
+#' "N12_2_1"
+#' @rdname
+#' "N12_2_2"
+#' @rdname
+#' "N12_2_10"
+#' @rdname
+#' "N12_10_0.1"
+#' @rdname
+#' "N12_10_0.5"
+#' @rdname
+#' "N12_10_1"
+#' @rdname
+#' "N12_10_2"
+#' @rdname
+#' "N12_10_10"
+#'
+#' @rdname
+#' "N24_0.1_0.1"
+#' @rdname
+#' "N24_0.1_0.5"
+#' @rdname
+#' "N24_0.1_1"
+#' @rdname
+#' "N24_0.1_2"
+#' @rdname
+#' "N24_0.1_10"
+#' @rdname
+#' "N24_0.5_0.1"
+#' @rdname
+#' "N24_0.5_0.5"
+#' @rdname
+#' "N24_0.5_1"
+#' @rdname
+#' "N24_0.5_2"
+#' @rdname
+#' "N24_0.5_10"
+#' @rdname
+#' "N24_1_0.1"
+#' @rdname
+#' "N24_1_0.5"
+#' @rdname
+#' "N24_1_1"
+#' @rdname
+#' "N24_1_2"
+#' @rdname
+#' "N24_1_10"
+#' @rdname
+#' "N24_2_0.1"
+#' @rdname
+#' "N24_2_0.5"
+#' @rdname
+#' "N24_2_1"
+#' @rdname
+#' "N24_2_2"
+#' @rdname
+#' "N24_2_10"
+#' @rdname
+#' "N24_10_0.1"
+#' @rdname
+#' "N24_10_0.5"
+#' @rdname
+#' "N24_10_1"
+#' @rdname
+#' "N24_10_2"
+#' @rdname
+#' "N24_10_10"
+#'
+#' @rdname
+#' "N12_3_0.1_0.1"
+#' @rdname
+#' "N12_3_0.1_0.5"
+#' @rdname
+#' "N12_3_0.1_1"
+#' @rdname
+#' "N12_3_0.1_2"
+#' @rdname
+#' "N12_3_0.1_10"
+#' @rdname
+#' "N12_3_0.5_0.1"
+#' @rdname
+#' "N12_3_0.5_0.5"
+#' @rdname
+#' "N12_3_0.5_1"
+#' @rdname
+#' "N12_3_0.5_2"
+#' @rdname
+#' "N12_3_0.5_10"
+#' @rdname
+#' "N12_3_1_0.1"
+#' @rdname
+#' "N12_3_1_0.5"
+#' @rdname
+#' "N12_3_1_1"
+#' @rdname
+#' "N12_3_1_2"
+#' @rdname
+#' "N12_3_1_10"
+#' @rdname
+#' "N12_3_2_0.1"
+#' @rdname
+#' "N12_3_2_0.5"
+#' @rdname
+#' "N12_3_2_1"
+#' @rdname
+#' "N12_3_2_2"
+#' @rdname
+#' "N12_3_2_10"
+#' @rdname
+#' "N12_3_10_0.1"
+#' @rdname
+#' "N12_3_10_0.5"
+#' @rdname
+#' "N12_3_10_1"
+#' @rdname
+#' "N12_3_10_2"
+#' @rdname
+#' "N12_3_10_10"
